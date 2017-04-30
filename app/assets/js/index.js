bd4c3823a2e8fe39d1cc02766236d99fd8e2329e
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2017-04-30 21:32:45 +0200

    Add plumber and Parker for SASS

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,4 +1,1 @@
-import a from './modules/test';
-
-
-console.log(a(24,45));
+console.log('Hi');
--- gulp_config/tasks/css.js
+++ gulp_config/tasks/css.js
@@ -1,27 +1,39 @@
 const gulp = require('gulp'),
+      fs = require('fs'),
       sass = require('gulp-sass'),
       autoprefixer = require('autoprefixer'),
       cssnano = require('cssnano'),
       postcss = require('gulp-postcss'),
-      postcssImport = require('postcss-import'),
+      Parker = require('parker/lib/Parker'),
+      scssParser = require('postcss-scss'),
+      plumber = require('gulp-plumber'),
       { css } = require('../paths');
 
 gulp.task('css:dev', cssDev);
 gulp.task('css:prod', cssProd);
-
+gulp.task('css:analyze', analyzeCSS);
 
 function cssDev() {
   return gulp.src(css.src)
-        .pipe(postcss([postcssImport]))
-        .pipe(sass())
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer]))
         .pipe(gulp.dest(css.dest));
 }
 
 function cssProd() {
   return gulp.src(css.src)
-        .pipe(postcss([postcssImport]))
-        .pipe(sass())
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer, cssnano]))
         .pipe(gulp.dest(css.prodDest));
+}
+
+function analyzeCSS(done) {
+  fs.readFile('./app/dist/css/styles.css', (err, data) => {
+    const parker = new Parker(require('parker/metrics/All'));
+    const results = parker.run(data.toString());
+    console.log(JSON.stringify(results, null, 2));
+    done();
+  })
 }
